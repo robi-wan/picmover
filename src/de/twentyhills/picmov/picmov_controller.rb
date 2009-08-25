5efa11f171fc7eea43754ccc61232db604a94ed5
robi-wan <robi-wan@suyu.de>
2009-08-26 00:43:52 +0200

    Update model properties on start_button_action_performed

--- src/de/twentyhills/picmov/picmov_controller.rb
+++ src/de/twentyhills/picmov/picmov_controller.rb
@@ -14,63 +14,45 @@ class PicmovController < ApplicationController
     signal(:init)
   end
 
-  def update_my_model
-    update_model(view_state.model, :source_folder, :target_folder)
-  end
 
   def source_folder_button_action_performed(event)
-    #todo update complete model
-    update_my_model()
+    update_model_properties()
     show_folder_chooser(:source_folder, event)
-#    update_model(view_state.model, :source_folder)
-#    folder_chooser = com.jidesoft.swing.FolderChooser.new()
-#    unless model.source_folder.empty? then
-#      current_folder = folder_chooser.getFileSystemView().createFileObject(model.source_folder)
-#    end
-#    folder_chooser.setCurrentDirectory(current_folder)
-#    folder_chooser.setFileHidingEnabled(true)
-#    result = folder_chooser.showOpenDialog(event.get_source().getTopLevelAncestor())
-#    if result == com.jidesoft.swing.FolderChooser::APPROVE_OPTION then
-#      selected_file = folder_chooser.getSelectedFile()
-#      if selected_file != nil then
-#        folder = selected_file.to_s()
-#      else
-#        folder = "";
-#      end
-#      model.source_folder=folder
-#      update_view
-#      # das Mapping funktioniert nur mittels der update-* - Aufrufe
-#      # Der Signalhandler ist an dieser Stelle unnötig
-#      #      transfer[:source_folder] = folder
-#      #      signal(:select_source_folder)
-#    end
   end
 
   def target_folder_button_action_performed(event)
-    #todo update complete model
-    update_my_model()
+    update_model_properties()
     show_folder_chooser(:target_folder, event)
-#    update_model(view_state.model, :target_folder)
-#    folder_chooser = com.jidesoft.swing.FolderChooser.new()
-#    unless model.target_folder.empty? then
-#      current_folder = folder_chooser.getFileSystemView().createFileObject(model.target_folder)
-#    end
-#    folder_chooser.setCurrentDirectory(current_folder)
-#    folder_chooser.setFileHidingEnabled(true)
-#    result = folder_chooser.showOpenDialog(event.get_source().getTopLevelAncestor())
-#    if result == com.jidesoft.swing.FolderChooser::APPROVE_OPTION then
-#      selected_file = folder_chooser.getSelectedFile()
-#      if selected_file != nil then
-#        folder = selected_file.to_s()
-#      else
-#        folder = "";
-#      end
-#      model.target_folder=folder
-#      # das Mapping funktioniert nur mittels der update-* - Aufrufe
-#      update_view
-#    end
   end
 
+
+  def stop_button_action_performed
+    # todo stop copying
+    java.lang.System.exit(0)
+  end
+
+  def start_button_action_performed(event)
+    update_model_properties()
+    # todo extends javax.swing.SwingWorker with a ruby class and let it execute moving...
+    signal(:start_moving)
+    repaint_while do
+      model.move_pictures() do |file, percent|
+        #if "progress".eql?(evt.property_name) then
+        transfer[:progress]= percent*100
+        transfer[:file]=file.source
+        signal(:progress)
+        #end
+      end
+      signal(:end_moving)
+    end
+  end
+
+  :private
+
+  def update_model_properties
+    update_model(view_state.model, :source_folder, :target_folder)
+  end
+  
   def show_folder_chooser(target, event)
     my_folder_chooser = folder_chooser()
     unless model.send(target).empty? then
@@ -90,26 +72,6 @@ class PicmovController < ApplicationController
       model.send("#{target}=", folder)
       # das Mapping funktioniert nur mittels der update-* - Aufrufe
       update_view
-    end
-  end
-
-  def stop_button_action_performed
-    # todo stop copying
-    java.lang.System.exit(0)
-  end
-
-  def start_button_action_performed(event)
-    # todo extends javax.swing.SwingWorker with a ruby class and let it execute moving...
-    signal(:start_moving)
-    repaint_while do
-      model.move_pictures() do |file, percent|
-        #if "progress".eql?(evt.property_name) then
-          transfer[:progress]= percent*100
-          transfer[:file]=file.source
-          signal(:progress)
-        #end
-      end
-      signal(:end_moving)
     end
   end
 
